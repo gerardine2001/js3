0d82d128594e6eeca6221c63e7e8c07b1de59f4f
gerardine <gerardinekaba@gmail.com>
2021-04-05 10:40:34 +0300

    practice3

---
+++ practice3.js
@@ -0,0 +1,10 @@
+var Capitals={rwanda:"kigali",kenya:"nairobi",Uganda:"Kampala"};
+console.log(Capitals);
+for(a in Capitals){
+    console.log("In" ,a, "we have ",Capitals[a]);
+    console.log(a);
+}
+var myObj=["laptop","books","headphones","charger"];
+for(let m=4;m>=0;m--){
+    console.log(m);
+}
